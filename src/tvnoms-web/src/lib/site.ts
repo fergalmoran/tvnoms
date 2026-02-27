fe02056b34393753f22d169979401e0f84291e05
Fergal Moran <fergal.moran@gmail.com>
2026-02-27 18:05:13 +0000

    Move web to tanstack

---
+++ src/tvnoms-web/src/lib/site.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "TV Noms";
+export const SITE_DESCRIPTION = "Om noms for your TV.";
+export const SITE_URL = "https://tvnoms.com";
---
+++ src/tvnoms-web/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+
+const config = defineConfig({
+  server: {
+    allowedHosts: ["localhost", "tvnoms.com", "tvnoms.dev.fergl.ie"],
+  },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
